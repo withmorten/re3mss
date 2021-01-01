7331ba3385d39c9e6028c8f214b98103f85b8f9d
Anonymous Maarten <anonymous.maarten@gmail.com>
2021-01-01 22:28:14 +0100

    Add conanfile

---
+++ conanfile.py
@@ -0,0 +1,42 @@
+from conans import ConanFile, CMake, tools
+from conans.errors import ConanInvalidConfiguration
+import textwrap
+
+
+class MilesSDKConan(ConanFile):
+    name = "miles-sdk"
+    settings = "os", "arch", "compiler", "build_type"
+
+    exports_sources = "CMakeLists.txt", "re3mss/mss.h", "re3mss/mss32.c"
+    generators = "cmake"
+    no_copy_source = True
+
+    def configure(self):
+        if self.settings.os != "Windows":
+            raise ConanInvalidConfiguration("miles-sdk is not available for os != Windows")
+
+    def build(self):
+        tools.save("CMakeLists.txt",
+                   textwrap.dedent(
+                       """
+                       cmake_minimum_required(VERSION 3.0)
+                       project(cmake_wrapper)
+
+                       include("{}/conanbuildinfo.cmake")
+                       conan_basic_setup(TARGETS)
+
+                       add_subdirectory("{}" re3mss)
+                       """).format(self.install_folder.replace("\\", "/"),
+                                   self.source_folder.replace("\\", "/")))
+        cmake = CMake(self)
+        cmake.configure(source_folder=self.build_folder)
+        cmake.build()
+
+    def package(self):
+        cmake = CMake(self)
+        cmake.install()
+
+    def package_info(self):
+        self.cpp_info.libs = ["mss32"]
+        self.cpp_info.names["cmake_find_package"] = "MilesSDK"
+        self.cpp_info.names["cmake_find_package_multi"] = "MilesSDK"
